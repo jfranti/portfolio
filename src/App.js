58a82f130493fb733d3d3344dc01a7b2b2b73693
john-franti <jfranti@gmail.com>
2020-02-12 11:36:56 -0800

    update components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ProjectContainer from "./containers/ProjectContainer";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <ProjectContainer />
       </header>
     </div>
   );
---
+++ src/containers/ProjectContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProjectContainer = () => {
+    return (
+        <div>
+            <p>Projects</p>
+        </div>
+    )
+}
+
+export default ProjectContainer
